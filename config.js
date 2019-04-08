d7547c26de0556a8b9e15dc2d8174bf6c8629674
Richard Ramos <info@richardramos.me>
2019-04-08 15:32:22 -0400

    Initial version

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'token': process.env.GITTER_TOKEN,
+  "gitterRoom": "status-im/gitter-bridge",
+  "statusRoom": "#gitter-bridge",
+  "statusProvider": "ws://localhost:8546",
+  "statusPrivateKey": process.env.STATUS_PRIVKEY
+}
