cc0bb9ceffb0578b4e9dc285c9b0d23b227c0fe3
Richard Ramos <info@richardramos.me>
2019-04-09 09:40:35 -0400

    fix: remove geth from start file

--- start.sh
+++ start.sh
@@ -3,8 +3,4 @@
 export GITTER_TOKEN=
 export STATUS_PRIVKEY=
 
-killall geth
-
-geth --testnet --ws --wsport=8546 --wsaddr=localhost --wsorigins=* --shh --wsapi=web3,net,shh --syncmode=light &
-
 npm start
